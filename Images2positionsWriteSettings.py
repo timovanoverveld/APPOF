47d988dd12fe4cacfe6619a6f559e3cc2aa6033c
Timo van Overveld <timovoverveld@live.nl>
2019-11-27 15:37:43 +0100

    Add file to write settings for Image2Positions

---
+++ Images2positionsWriteSettings.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Timo van Overveld, 2019
+
+"""Write the settings of the Images2positions script to a file"""
+
+# Imports
+import fnmatch
+import json
+import argparse
+
+def main():
+    """Launcher."""
+
+    # Argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    parser.add_argument('-f', help="destination file")
+    args = parser.parse_args()
+
+    if args.verbose:
+        print("verbosity turned on")
+
+    # Adjustable settings
+    settings = {
+        #Directories
+        'basedir' : '/PhD/Experimenten/Jeroen/26-11-2019/',
+        'calibrationdir' : 'Calibration/',
+        'measurementdir' : 'Wave with pa\rticles/',
+
+        # refraction indices
+        'nair'   : 1.00027717,
+        'nwater' : 1.330,
+
+        # Line spacing
+        'linespacing' : 0.025, #[m]
+        'linespacingpx' : 75, #[px]
+
+        # Channel width
+        'channelwidth' : 0.1, #[m]
+
+        # Cropping bounds
+        'bounds' : [405, 405, 0, 0], # Top, bottom, left, right
+        # Central pixels (no lines present)
+        'centerpx' : [70,-60],
+
+        # Thresholding values
+        'thresholdvalue' : 720,
+
+        # Fitting order
+        'warpingorder' : 3,
+        'surfaceshapeorder' : 10,
+
+        'plots'   : True,
+        'verbose' : True
+    }
+
+    if args.f:
+        settingsfile = args.f
+    else:
+        settingsfile = 'settings.txt'
+
+    f = open(settingsfile, 'w+')
+    f.write(json.dumps(settings))
+
+    print('Constants stored in',settingsfile)
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # APPOF
- Analysis of Particles and Patterns in Oscillating Flows
+ Analysis of Particles and Patterns in Oscillating Flows (APPOF) is a Python toolbox that can be used to analyze visual experimental data.
