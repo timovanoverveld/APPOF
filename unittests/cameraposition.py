0df5c5e1090336e52dd8f9c270af5a466e1c2100
Timo van Overveld <timovoverveld@live.nl>
2019-11-29 16:05:22 +0100

    Add camera position unit test

--- unittests/cameraposition.py
+++ unittests/cameraposition.py
@@ -4,111 +4,80 @@
 # Timo van Overveld, 2019
 
 """A unittest to check whether the camera position is consistently found"""
-# Input: directory locations with images (.tif)
-# Output: data structure with particle positions in real space
+# Input: settings_cameraposition.txt, H0.tif, H7.tif
+# Output: Camera position
 
 # Imports
-import argparse
 import numpy as np
-import cv2
-from matplotlib import pyplot as plt
-import scipy.optimize as optimization
-from scipy.signal import find_peaks
-from scipy import ndimage
-import os
-import fnmatch
 import json
-
+import unittest
 from images2positions_functions import *
 
 ############################################################################
 
+class TestCalibration(unittest.TestCase):
 
-def main():
+    def test_cameraposition(self):
+        # Reading settings file
+        f = open('settings_cameraposition.txt', 'r')
+        settings = json.loads(f.read())
+       
+        # Check if settings exists
+        self.assertTrue(settings)
 
-    # Reading settings file
-    f = open(args.f, 'r')
-    settings = json.loads(f.read())
+        # refraction indices
+        nair   = settings['nair']
+        nwater = settings['nwater']
+        n      = nair/nwater
 
-    # Directories
-    basedir = settings['basedir']
-    calibrationdir = basedir + settings['calibrationdir']
+        # Line spacing (real world) [m]
+        linespacing = settings['linespacing']
+        linespacingpx = settings['linespacingpx']
 
-    # refraction indices
-    nair   = settings['nair']
-    nwater = settings['nwater']
-    n      = nair/nwater
+        # Cropping bounds (top, bottom, left, right)
+        bounds = settings['bounds']
 
-    # Line spacing (real world) [m]
-    linespacing = settings['linespacing']
-    linespacingpx = settings['linespacingpx']
+        # Central pixels to exclude
+        centerpx = settings['centerpx']
 
-    # Channel width [m]
-    channelwidth = settings['channelwidth']
+        # Fitting order (C*x^order+...)
+        warpingorder = settings['warpingorder']
 
-    # Cropping bounds (top, bottom, left, right)
-    bounds = settings['bounds']
+        ################################### 
+        # Warping
+        calAlist = 'H0.tif' 
 
-    # Central pixels to exclude
-    centerpx = settings['centerpx']
+        # Camera position
+        calBlist = ['H0.tif','H7.tif']
+   
+        Hlist = np.asarray([0,7])
+        #############################################
 
-    # Fitting order (C*x^order+...)
-    warpingorder = settings['warpingorder']
+        # Camera warping
+        # Linking the pixels to the real world coordinates, because the camera warps the image. This script gives the transformation from pixels to real world coordinates for a water height of 0.
+       
+        #Obtain line positions
+        xpix, Hreal = pixHlist(calAlist,Hlist,bounds=bounds,centerpx=centerpx,linespacingpx=linespacingpx)
+        xreal, Nlines = clusterlines(xpix,linespacing,Nlines=17)
+        
+        # Fit the warping function through the data
+        pix2realx = pixrealfit(xpix, xreal, warpingorder)
+        ###########################
 
-    #####################################
+        # Camera position
+        # Finding the camera position by using known flat water heights.
 
+        xpix, H = pixHlist(calBlist,Hlist,bounds=bounds,centerpx=centerpx,linespacingpx=linespacingpx)
+        xprojected = pix2realx(xpix)
+        xreal, Nlines = clusterlines(xpix,Nlines=Nlines,linespacing=linespacing)
+     
+        xc, Hc = cameraposition(xprojected,xreal,H,n)
 
-    # Filelist
-    # Allocation
-    flist = np.empty(0,dtype=int)
-    Hlist = np.empty(0,dtype=int)
-
-    for (dirpath, dirnames, filenames) in os.walk(calibrationdir):
-        if np.size(fnmatch.filter([dirpath],'*H*'))==1:
-            f = np.asarray(fnmatch.filter(filenames,'*.tif*'))
-            flist = np.append(flist,f)
-
-            if np.size(f) > 0:
-                H = dirpath[dirpath.find('H')+1:]
-                for i in range(0,np.size(f),1):
-                    Hlist = np.asarray(np.append(Hlist,int(H)))
-
-    # Warping list
-    calAlist = [calibrationdir + 'H0/' + i for i in flist[Hlist==0]]
-
-    # Camera position list
-    calBlist = [calibrationdir + 'H' + str(Hlist[i]) + '/' + flist[i] for i in range(0,np.size(flist))]
-
-    #############################################
-
-    # Camera warping
-    # Linking the pixels to the real world coordinates, because the camera warps the image. This script gives the transformation from pixels to real world coordinates for a water height of 0.
-
-    #Obtain line positions
-    xpix, Hreal = pixHlist(calAlist,Hlist,bounds=bounds,centerpx=centerpx,linespacingpx=linespacingpx)
-
-    xreal, Nlines = clusterlines(xpix,linespacing)
-
-    # Fit the warping function through the data
-    pix2realx = pixrealfit(xpix, xreal, warpingorder)
-
-    # Now a similar thing for the y-coordinate, across the width of the channel
-    imagesize = np.shape(readcropimage(calAlist[0],bounds=bounds))
-    pix2realy = pixrealfit([0,imagesize[0]],[0,channelwidth],1)
-
-    ###########################
-
-    # Camera position
-    # Finding the camera position by using known flat water heights.
-
-    xpix, H = pixHlist(calBlist,Hlist,bounds=bounds,centerpx=centerpx,linespacingpx=linespacingpx)
-    xprojected = pix2realx(xpix)
-    xreal, Nlines = clusterlines(xpix,Nlines=Nlines,linespacing=linespacing)
- 
-    xc, Hc = cameraposition(xprojected,xreal,H,n)
-
-    #################################
-
+        #################################
+        # xc should be 0.35000
+        # Hc should be 1.82484
+        self.assertAlmostEqual(xc[0],0.35000,places=5)
+        self.assertAlmostEqual(Hc[0],1.82484,places=5)
 
 if __name__ == '__main__':
     unittest.main()
