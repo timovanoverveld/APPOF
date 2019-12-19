ff13cf14752f66638088075c333916c2ce009e31
Timo van Overveld <timovoverveld@live.nl>
2019-12-19 13:27:05 +0100

    Add jupyter notebook contents to create first draft

--- trajectories.py
+++ trajectories.py
@@ -19,7 +19,110 @@ import argparse
 ############################################################################
 def trajectories():
     
-    print('Hello world')
+    if Windows:
+        file = '/PhD/Experimenten/Jeroen/STRM to images/500ms_watershed/Positions.npz'    
+    else:
+        file = '/media/timo/Backup Plus/PhD/Experimenten/Jeroen/STRM to images/500ms_watershed/Positions.npz'
+    
+    np.set_printoptions(threshold=np.inf)
+    
+    with np.load(file) as data:
+        X = data['x']
+        Y = data['y']
+        time = data['t']
+    
+        x = X[:,:]
+        y = Y[:,:]
+    
+        # Number of detected particles
+        N = [np.count_nonzero(k) for k in x]
+    
+        N_timesteps = np.size(time) # Number of timesteps
+        
+        # Maximum number of particles
+        N_max = 500
+        I = np.linspace(0,N_max-1,N_max,dtype=int)
+        ind_loop = np.linspace(0,N_max-1,N_max,dtype=int) # Indices to include in the calculation
+    
+        # All information of the particles will be stored here
+        Particles = np.zeros((N_max,N_timesteps,2),dtype=float) # Number of particles X Number of timesteps X coordinates (x,y)
+    
+        # Store the first particles
+        Particles[:,0,0] = x[0,:] # x at time 0
+        Particles[:,0,1] = y[0,:] # y at time 0
+        
+        Distance_thres = 20 # Distance threshold in pixels
+        
+    for t in range(0,20,1):
+        # Choose particle i
+        idx_fw = np.zeros(N_max,dtype=int)
+        idx_bw = np.zeros(N_max,dtype=int)
+    
+        # Loop over particles
+        for i in ind_loop: #range(0,500,1):
+            if i >= 0:
+                # Forward time distances
+                # Calculate distance to all other particles
+                Distance_fw = np.sqrt((Particles[i,t,0]-x[t+1,ind_loop])**2+(Particles[i,t,1]-y[t+1,ind_loop])**2)
+    
+                # Sort this matrix, to find the shortest distance in the next timestep
+                idx_fw[i] = np.argsort(Distance_fw)[0]
+    
+                # Backward time distances
+                # Calculate distance to all other particles
+                Distance_bw = np.sqrt((x[t+1,i]-Particles[ind_loop,t,0])**2+(y[t+1,i]-Particles[ind_loop,t,1])**2)
+    
+                # Sort this matrix, to find the shortest distance in the next timestep
+            #     idx2 = np.argsort(Distance_bw)[0]
+                idx_bw[i] = np.argsort(Distance_bw)[0]    
+    
+        # Find the parts where we have closed loops, store by index at t0 and t1
+        closed_loop_check = idx_bw[idx_fw]-I
+        a = np.where(closed_loop_check==0)[0] # The positions in idx_fw for which we have found a closed loop
+        b = idx_fw[a] # The positions they point to
+    
+        # Add to particles already
+        Particles[a,t+1,0] = x[t+1,idx_fw[a]]
+        Particles[a,t+1,1] = y[t+1,idx_fw[a]]
+    
+        # Check for distances between found particles, if distance is too large then particles are not the same
+        Distances = np.sqrt((Particles[:,t,0]-Particles[:,t+1,0])**2+(Particles[:,t,1]-Particles[:,t+1,1])**2)
+        q = np.where(Distances>=Distance_thres)[0]
+    
+        #First zero in particles: search backwards, find last nonzero 
+        nz = np.nonzero(Particles[:,t+1,0])[0][-1] # Last nonzero, so new particle should be placed in 1 further (hence the +1)
+    
+    #     print('nz, q',nz,q)
+    #     print(Particles[nz+1:nz+1+np.size(q),t+1,0],Particles[q,t+1,0])
+    
+        # Put discrepant particles at the back
+        Particles[nz:nz+np.size(q),t+1,:] = Particles[q,t+1,:]
+    
+        # Remove particles from previous locations:
+        Particles[q,t+1,:] = 0
+    
+    #     print(Particles[nz:nz+1+np.size(q),t+1,0])
+    
+    
+    plt.figure(figsize=(20,10))
+    imax = 20
+    for i in range(0,imax):
+        plt.scatter(x[i,:],y[i,:],s=5,color=(i/imax,1-i/imax,0))
+    
+    for i in range(0,500,1):
+        nonzeros = np.argwhere(Particles[i,:,0] !=0 )
+        if np.size(nonzeros)==0:
+            break
+        p1 = int(nonzeros[0])
+        p2 = int(nonzeros[-1])
+        plt.plot(Particles[i,p1:p2,0],Particles[i,p1:p2,1],'k',Linewidth=3)
+    
+    plt.xlim(1000,1200)
+    plt.ylim(0,350)
+    
+    plt.show()
+    
+    print('done')
 
 
 if __name__ == "__main__":  
